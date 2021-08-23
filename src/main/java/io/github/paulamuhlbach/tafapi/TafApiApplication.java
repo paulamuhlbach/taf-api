2ab98c4a0f0a13715d0461b1823a4760b20c966d
paula <pollam@gmail.com>
2021-08-23 08:28:23 -0300

    primeiro commit mysql

--- src/main/java/io/github/paulamuhlbach/tafapi/TafApiApplication.java
+++ src/main/java/io/github/paulamuhlbach/tafapi/TafApiApplication.java
@@ -2,13 +2,12 @@ package io.github.paulamuhlbach.tafapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-																																																
+
 @SpringBootApplication
 public class TafApiApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(TafApiApplication.class, args);
-		
 	}
 
-}	
+}
--- src/main/java/io/github/paulamuhlbach/tafapi/repository/EstadoRepository.java
+++ src/main/java/io/github/paulamuhlbach/tafapi/repository/EstadoRepository.java
@@ -3,7 +3,6 @@ package io.github.paulamuhlbach.tafapi.repository;
 import io.github.paulamuhlbach.tafapi.model.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
 
 }
