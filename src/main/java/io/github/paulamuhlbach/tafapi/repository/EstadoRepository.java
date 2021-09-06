566a75b99b94f126a9798e94fad61e6aa2ba6176
paula <pollam@gmail.com>
2021-09-06 19:16:35 -0300

    ajustes no projeto

--- src/main/java/io/github/paulamuhlbach/tafapi/repository/EstadoRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.paulamuhlbach.tafapi.repository;
-
-import io.github.paulamuhlbach.tafapi.model.Estado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstadoRepository extends JpaRepository<Estado, Long> {
-
-}
